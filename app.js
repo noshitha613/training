08313551a0de6cc30c19ec66dfdec4716b127688
noshitha613 <noshithanoshitha@gmail.com>
2025-01-09 18:36:40 +0530

    day

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
+    } else if (i % 3 === 0) { 
       console.log('Fizz');
     } else if (i % 5 === 0) {
       console.log('Buzz');
@@ -9,3 +9,4 @@ for (let i = 1; i <= 100; i++) {
       console.log(i);
     }
   }
+  
---
+++ functions.js
@@ -0,0 +1,4 @@
+let x=_=>{console.log("Hi");
+         console.log("Hello");
+         console.log("Bye");}
+         x();
