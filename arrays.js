a2eaa527f39497d7a3921520fa34a1b569eac934
noshitha613 <noshithanoshitha@gmail.com>
2025-01-21 13:04:24 +0530

    day

--- arrays.js
+++ arrays.js
@@ -1,6 +1,19 @@
-let users = [ "Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
-let x = users.forEach((user)=>{
-    return user;
+// let users = [ "Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+// let x = users.forEach((user)=>{
+//     return user;
 
-});
-console.log(x);
+// });
+// console.log(x);
+
+let userDetails={
+    name:"ram",
+    age:25,
+    city:"hyd"
+
+};
+console.log(userDetails);
+let x=JSON.stringify(userDetails);
+console.log("JSON object "+x);
+let y=JSON.parse(x);
+console.log(y);
+
---
+++ async.js
@@ -0,0 +1,8 @@
+console.log(10);
+console.log(20);
+setInterval(()=>{
+    document.writeln("MRU");
+    console.log(30);
+},1500)
+console.log(40);
+console.log(50);
