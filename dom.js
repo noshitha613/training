8edd9f9eb821f30b67ab72727300ce6c17ef6171
noshitha613 <noshithanoshitha@gmail.com>
2025-01-28 13:01:53 +0530

    day

--- dom.js
+++ dom.js
@@ -28,49 +28,51 @@
 //     console.log(userDetails);
 //     localStorage.setItem("userData" ,JSON.stringify(userDetails))
 // })
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
 
-let mainEle = document.createElement("div");
-mainEle.setAttribute("id","mainBlock");
-mainEle.style.border="2px solid red";
-mainEle.style.width="550px";
-mainEle.style.height="550px";
+let gender= document.getElementsByName("gender");
 
- console.log(mainEle);
+//! hiding and showing the password values
+check.addEventListener("click", event=>{
+    // console.log(event.target.checked);
+    if(event.target.checked == true){
+    password.setAttribute("type", "text");
+    show.innerText = "hide password";
+}else{
+    password.setAttribute("type", "password");
+    show.innerText = "show password";
+    }
+    
+    })
+// console.log(gender);
+form.addEventListener("submit", event=>{
+event.preventDefault();
+let un= username.value;
+let up =password.value;
+let gen = "";
+for(let i=0;i<=gender.length-1;i++){
+// console.log(gender[i].value);
+// console.log(gender[i].checked);
+if(gender[i].checked == true) {
+// console.log(gender[i].value);
+gen =gender[i].value;
 
-let topEle = document.createElement("div");
- topEle.setAttribute("class", "topBlock");
- // console.log(topEle);
- let image = document.createElement("img");
- image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
- image.style.width="550px"
+}
 
-let bottomEle = document.createElement("div");
-bottomEle.setAttribute("class", "bottomBlock");
-// console.log(bottomEle);
+}
+// console.log(un up, gen);
 
-let h1= document.createElement("h1");
-h1.innerText = "CAT";
-h1.style.textAlign="center"
+let userDetails={
+username: un,
+Password:up,
+gender: gen
 
-let btn = document.createElement("button");
-btn.innerText="View More";
-btn.style.border="none";
-btn.style.padding="20px";
-btn.style.backgroundColor="dodgerblue";
-btn.style.color="white";
-bottomEle.appendChild(h1);
-bottomEle.appendChild(btn);
-let btn = document.createElement("button");
-btn.innerText="View More";
-btn.style.border="none";
-btn.style.padding="20px";  
-btn.style.backgroundColor="dodgerblue";
-btn.style.color="white";
+}
+console.log(userDetails);
+sessionStorage.setItem("userData", JSON.stringify(userDetails))
 
-bottomEle.appendChild(h1);
-bottomEle.appendChild(btn);
-
-topEle.appendChild(image);
- mainEle.appendChild(topEle);
- mainEle.appendChild(bottomEle);
- document.body.appendChild(mainEle);
+})
